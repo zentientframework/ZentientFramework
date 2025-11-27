4708f5e6e69c2814c8074f1dbdf3ea23a62dc4bd
zentientframework <zentientframework@gmail.com>
2025-11-27 13:48:26 +0100

    feat(Exceptions): Add DefaultConstructorMissingException class
    Introduce a new internal exception class, `DefaultConstructorMissingException`, in the `Zentient.Exceptions` namespace.
    
    - Added `DefaultConstructorMissingException.cs` with a copyright header under the MIT license.
    - The class inherits from `Exception` and provides a constructor that formats a custom error message for missing default constructors.

---
+++ src/Zentient.Core/Exceptions/DefaultConstructorMissingException.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="Code.cs" author="Zentient Framework Team">
+// (c) 2025 Zentient Framework Team. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+
+namespace Zentient.Exceptions
+{
+    internal class DefaultConstructorMissingException : Exception
+    {
+        public DefaultConstructorMissingException(string key, string message) : base($"Failed to resolve definition for key '{key}': {message}")
+        { }
+    }
+}
